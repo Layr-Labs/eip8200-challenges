836bf22bf7355131fe3222d29461d3fca373a382
Leo Alt <leo@powdrlabs.com>
2026-08-06 14:48:06 +0200

    Prove the BLAKE2f source compiler bridge

--- Challenge/Blake2f/ProofSupport.lean
+++ Challenge/Blake2f/ProofSupport.lean
@@ -2,6 +2,7 @@ import Challenge.Blake2f.ProofSupport.Bytecode
 import Challenge.Blake2f.ProofSupport.Algorithm
 import Challenge.Blake2f.ProofSupport.InitialState
 import Challenge.Blake2f.ProofSupport.Word
+import Challenge.Blake2f.ProofSupport.Yul
 
 set_option warningAsError true
 
